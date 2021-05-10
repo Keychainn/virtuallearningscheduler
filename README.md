c9bba66c6c07d0e120d92e808e873f1bb600bba8
Keychainn <sunbakedcow@gmail.com>
2021-05-10 19:56:28 -0400

    add basic functionality

---
+++ README.md
@@ -0,0 +1,25 @@
+## Stuy virtual learning scheduler for Windows
+---
+#### What is it?
+- I always get a a jolting sense of dread when I'm a minute late to class. Maybe I forgot about the time. However, this shall plague Stuy students no more. With this program, your zoom/google meets link will automatically launch, along with any links to attendance questions or student.desmos.com, jupitered.com, etc. Please excuse the uncommented code; I have been making this for 2 hours on on end.
+---
+#### Requirements:
+- Must be running Windows. I have not built in functionality for macOS/linux yet.
+- Launches Chrome, so you should have that installed.
+---
+#### Instructions and Info:
+- If using Thonny, go into Tools > open system shell and type `python scheduler.py` to run. Make sure you are in the right directory if there is an error.
+- If you are just starting out, you will see some introductory messages. After you press enter, you will be shown input areas for the name of the class, zoom/meets link, and any other additional links you may need that are specific to the class, such as your Google Classroom stream, jupitered attendance questions, etc.
+- Once you finish the process, "data.json" will be created in the same directory. If you ever wish to remake your class data, you can either edit directly inside of the JSON file or delete it; upon running scheduler.py again after deleting it, you can go through the data creation process again.
+- After you are satisfied with your data file, run the program again. It will open class links 2 minutes before class starts.
+- If the program quits immediately, you are likely running it after the last period has started.
+---
+#### Limitations:
+- Most obviously, this is only for Windows. I might make a MacOS version soon.
+- This doesn't account for double periods or gym and AB days yet. For doubles, you will have to put both gym and zoom link at the gym time slot, and the zoom link (for science, usually) on its regular slot. If it gets annoying by launching links in the middle of your double, you can do ctrl+C in the terminal to quit the process. The program will continue where it left off whenever you relaunch it. THIS DOES NOT CARRY OVER TO THE NEXT DAY.
+- Like I said in caps, this does not carry over to the next day. I think I will implement that in the next version. For now, running at the beginning of the day (or anytime in the middle of the school day) will work.
+- Some text says command/control, but this is only for Windows currently, so disregard "command" as it doesn't apply to Windows machines. Foreshadowing? maybe.
+- (dev note) I am not sure if sleep is the best way to do the waiting. Perhaps await or something similar may work?
+---
+#### How to steal my code:
+- On line 70, it has `periods=((9,8),(10,13),(11,18),(12,23),(13,28))`. If you want to adapt this code for personal use or for other schedules, add/remove/edit the tuples in `(militaryhour,minute)` format in chronological order.
---
+++ scheduler.py
@@ -0,0 +1,107 @@
+from time import sleep
+import json
+from os import system
+from datetime import datetime
+
+def checkIfElementsIn(userTuple,userInput):
+    conditional=False
+    for x in userTuple:
+        if x == userInput.lower():
+            conditional=True
+    return conditional
+
+def createData(): #TODO mention gym
+    print("\nRemember that days alternate between pds 1-5 and 6-10!")
+    print("Press command/control+c to exit at any time. does not work on thonny or other Python IDE; use the stop button instead.")
+    sleep(1)
+
+    pdDict={}
+
+    for classNum in range(10):
+        subDict={} # inside to flush when frees are in middle of day
+        print("\nWhat class do you have for period {}? Type \"N/A\" or \"free\" if you have a free or lunch during this period. Do not repeat subject names.".format(classNum+1))
+        selectedClass = input("> ")
+        
+
+        freePeriod = checkIfElementsIn(("free","na","n/a"),selectedClass)
+
+        if not freePeriod:
+            invalid=True
+            while invalid:
+                print("    Paste zoom/google meets link: (try: right clicking, command+shift+v, control+shift+v, control/command+v)")
+                zoom = input("    > ")
+                if "https" in zoom and ("zoom" in zoom or "meet" in zoom):
+                    invalid=False
+                else:
+                    print("That doesn't seem to work. Please try again with the full zoom link.")
+                    sleep(1)
+        
+            moreLinks=True
+            extra=[]
+            linkNum=1
+            while moreLinks:
+                print("    Any other links you regularly visit? (i.e. attendance questions) CHOOSE y/n :")
+                
+                yn = input("    > ")
+                if checkIfElementsIn(("n","no"),yn):
+                    moreLinks=False
+                elif checkIfElementsIn(("y","yes"),yn):
+                    print("        Paste link #{} here: (Be sure to paste links that are regularly accessible! i.e. rocket chat stream, Google Classroom stream)".format(linkNum))
+                    extra.append(input("        > "))
+                else:
+                    print("        \nGive a yes or no answer\n")
+                    linkNum-=1
+                linkNum+=1
+            subDict.update({"name":selectedClass,"zoom":zoom,"additional":extra})
+        else:
+            subDict.update({"name":"free"})
+        subDict.update({"free":freePeriod})
+
+        pdDict[classNum+1] = subDict # see if i can write as int
+    
+    with open("./data.json","w+") as writefile:
+        json.dump(pdDict,writefile)
+
+def makeDatetimeObj(hrmin):
+    return datetime(datetime.now().year,datetime.now().month,datetime.now().day,hrmin[0],hrmin[1])
+
+def commands(pdDict):
+    periods=((9,8),(10,13),(11,18),(12,23),(13,28)) # in military time
+    day = input("Is today a 1-5 or 6-10 day? Type 1 or 6: ")
+    if int(day) in (1,6):
+        if int(day)==6:
+            addon=5
+        else:
+            addon=0
+
+        period=1
+        for time in periods:
+            time_delta=(makeDatetimeObj(time)-datetime.now()).total_seconds()
+            if time_delta < 0:
+                period+=1
+                continue
+            print("Waiting until the start of the next period...")
+            sleep(time_delta)
+
+            if not pdDict[str(period+addon)]['free']: # check if free
+                print("Opening class links for period {}: {}".format(period+addon,pdDict[str(period+addon)]["name"]))
+                system("start chrome {}".format(pdDict[str(period+addon)]['zoom']))
+                for link in pdDict[str(period)]["additional"]:
+                    if link:
+                        system(f"start chrome {link}")
+
+            period+=1
+    else:
+        print("You did not type 1 or 6.")
+        commands(pdDict)
+
+
+if __name__=="__main__":
+    try:
+        with open("data.json") as json_file:
+            data=json.load(json_file)
+            commands(data)
+    except:
+        print("It looks like a data file doesn't exist (yet). Please contact qsun30@stuy.edu if this problem persists. If this is your first time using this program, this is expected!")
+        input("Press enter to start inputting classes, or command/control+C to exit. This program works best if run in Windows Powershell or Windows Command Prompt.")
+        createData()
